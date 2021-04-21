ed3fdee974637f25ff002ed2e0a46c662ba2907f
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-04-21 20:06:46 +0430

    Update tutorial.lua

--- tutorial.lua
+++ tutorial.lua
@@ -1,1 +1,3 @@
-
+-- it's comment
+print ("Hello , World") -- printing << Hello , World >> with insert a newline
+io.write("Hello , World") -- printing << Hello , World >> **without** insert a newline 
