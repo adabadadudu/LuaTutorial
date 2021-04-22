daae5f34c381aca9d41967f2c82b85a6e5372cbc
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-04-22 11:07:57 +0430

    Update tutorial.lua

--- tutorial.lua
+++ tutorial.lua
@@ -1,3 +1,14 @@
 -- it's comment
 print ("Hello , World") -- printing << Hello , World >> with insert a newline
 io.write("Hello , World") -- printing << Hello , World >> **without** insert a newline 
+--[[ 
+multiline comment 
+--]]
+-- variables
+local a,b,c
+a = 10
+b = 20
+c = 30
+
+print("Variables A is of type",type(a),"and have the",a,"value")
+--[[ if else # you can write a line only in a multiline comment like /* Hello World */ --]]
